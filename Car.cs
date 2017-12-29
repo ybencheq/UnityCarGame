c102d7e5e0af30e9160e0dec127042f0174f9480
Yasmine <31421189+ybencheq@users.noreply.github.com>
2017-12-29 13:17:29 -0500

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("up"))
+			transform.Translate(Vector3.forward*10f*Time.deltaTime); //frame dependent speed
+
+		if (Input.GetKey("down"))
+			transform.Translate(Vector3.back*10f*Time.deltaTime); //frame dependent speed
+
+		if (Input.GetKey("left"))
+			transform.Rotate(Vector3.up, -50f*Time.deltaTime); //frame dependent speed
+
+		if (Input.GetKey("right"))
+			transform.Rotate(Vector3.up, 50f*Time.deltaTime); //frame dependent speed
+		
+	}
+}
